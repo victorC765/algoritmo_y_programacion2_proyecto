55d155ac92da692a617efdaf439383a364b0d239
unknown <vc787259@gmail.com>
2024-04-08 17:24:40 -0400

    arreglo

---
+++ alp_2_proyecto_laravel/app/Http/Controllers/NominaController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class NominaController extends Controller
+{
+    public function index(Request $request)
+    {
+        $datan = DB::select("SELECT es.idestudiantes, p.nombre, p.apellido, p.cedula, p.fecha_nacimiento
+        FROM estudiantes es 
+        JOIN personas p ON p.idpersonas = es.personas_idpersonas;");
+        $datae = DB::select("SELECT * FROM `evaluaciones` WHERE 1");
+        $topCa = DB::select('SELECT p.nombre, p.apellido, p.cedula, es.idestudiantes, ev.tema,ev.tipo_evaluacion, n.calificacion
+        FROM estudiantes es
+        JOIN personas p ON es.personas_idpersonas = p.idpersonas
+        JOIN estudiantes_evaluaciones n ON es.idestudiantes = n.estudiantes_idestudiantes
+        JOIN evaluaciones ev ON n.evaluaciones_idevaluaciones = ev.idevaluaciones
+        
+        WHERE es.idestudiantes = ?
+        ORDER BY ev.idevaluaciones ASC', [ $request -> idestudiantes]);
+    
+        return view("nomina", ['est' => $datan, 'eval' => $datae, 'cali' => $topCa]);
+    }
+
+    public function create(Request $request){
+        try{
+           $sql=DB::insert("INSERT INTO `estudiantes_evaluaciones`( `estudiantes_idestudiantes`, `evaluaciones_idevaluaciones`, `calificacion`)  values(?,?,?)",
+           [
+             $request->idestudiantes,
+             $request->idevaluaciones,
+             $request->nota,
+            
+           ]);
+        }catch(\Throwable $th){
+            $sql = 0;
+         }
+          if($sql == true){
+            return back()->with("añadido","evaluación añadida correctamente");
+          }else{
+            return back()->with("error","Error al añadir la evaluación");
+          }
+    }
+}
---
+++ alp_2_proyecto_laravel/resources/views/nomina.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/nomina.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <title>Nomina Estudiantil</title>
+</head>
+<body>
+  <header>
+    <button class="btn btn-lg" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"> ≡ </button>
+
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Lista de Navegación</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <a href="./evaluacion"><button class="btn btn-success"> <img src="{{ asset('img/evaluation_8921149.png') }}" alt="icono de evaluacion" width="135" height="135" /></button></a>
+  </div>
+</div>
+  </header>
+  <div class="cont">
+  <h1 class="n"><img width="80" height="80" src="https://img.icons8.com/plasticine/100/graduation-cap.png" alt="graduation-cap"/>Estudiantes</h1>
+    <div class="box">
+      <div class="sel">
+      <div class="form-floating">
+        <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+          <option selected>Selecione una sección</option>
+          <option value="1">Sección A</option>
+          <option value="2">Sección B</option>
+          <option value="3">Sección C</option>
+        </select>
+        <label for="floatingSelect">Secciones</label>
+      </div>
+      <button class="btn btn-danger">🔍</button>
+    </div>
+    <table class="table  table-hover" style="margin: 0%">
+        <thead>
+          <tr class="table-primary">
+            <th scope="col" class="p-3">#</th>
+            <th scope="col" class="p-3">Nombre</th>
+            <th scope="col" class="p-3">Apellido</th>
+            <th scope="col" class="p-3">Cedula</th>
+            <th scope="col" class="p-3">Calificaciones</th>
+           
+          </tr>
+        </thead>
+        @foreach ($est as $estudiante)
+        <tbody>
+           
+            <tr>
+                <td>{{ $estudiante->idestudiantes }}</td>
+                <td>{{ $estudiante->nombre }}</td>
+                <td>{{ $estudiante->apellido }}</td>
+                <td>{{ $estudiante->cedula }}</td>
+                <td class="d-flex align-items-center">
+                  
+               
+                <form action="/lista" method="GET"  id="miFormulario">
+                  <div>
+                      <input type="hidden" value="{{ $estudiante->idestudiantes }}" name="idestudiantes">
+                  </div>
+                  <button type="submit" class="btn btn-warning m-1"><img width="40" height="40" src="https://img.icons8.com/dusk/64/upload--v1.png" alt="upload--v1"/></button>
+                </form>
+               
+                  <button type="submit" class="btn btn-info m-1" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $estudiante->idestudiantes }}">
+                  <img width="40" height="40" src="https://img.icons8.com/dusk/64/visible--v1.png" alt="visible--v1"/>
+                </button>
+             
+             
+                <div class="modal fade" id="exampleModal{{ $estudiante->idestudiantes }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-fullscreen">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel" >{{$estudiante->nombre}} {{$estudiante->apellido}}</h1> 
+                        <button type="button " class="btn-close btn-danger" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                     
+                      <div class="modal-body">
+                        <h4>caficaciones</h4>
+                     
+                              <div >
+                                <table class="table">
+                                  <thead>
+                                    <tr>
+                                    <th>evaluaciones</th>
+                                    <th>tipo de evaluación</th>
+                                    <th>Acción</th>
+                                  </tr>
+                                  </thead>
+                                 
+                                  <tbody>
+                                    @foreach ($eval as $item)
+                                <tr>
+                                <td>{{$item->tema}}</td>
+                                  <td>{{$item->tipo_evaluacion}}</td>
+                                   <td><button type="button" class="btn btn-primary" onclick="abrirSegundaModal('#modalPequeña{{$item->idevaluaciones}}{{ $estudiante->idestudiantes }}')">
+                                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/plus.png" alt="plus"/>
+                                  </button>
+                                  <div class="modal fade"  id="modalPequeña{{$item->idevaluaciones}}{{ $estudiante->idestudiantes }}" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="ModalPequeñaLabel">
+
+                                    <div class="modal-dialog modal-sm">
+                                      <div class="modal-content">
+                                      <div class="fondo-difuminado">
+                                        <div class="modal-header">
+                                          <h1 class="modal-title fs-5" id="exampleModalLabel">Cargar Nota</h1>
+                                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                           <form action="{{route("nomina.create")}}" method="post">
+                              @csrf
+                              <div>
+                                <input  type="hidden" value="{{$item->idevaluaciones}}" name="idevaluaciones" >
+                              </div>
+                                  <div class="i">
+                                <input  type="hidden" value="{{$estudiante->idestudiantes}}" name="idestudiantes">
+                              </div>
+                              
+                                <div>
+                                  <label for="" >nota</label>
+                                <input type="number" name="nota" min="0" max="100">
+                                <button class="btn btn-info" type="submit">cargar</button>
+                                </form>
+                              </div>
+                            </div>
+                          </div>
+                                        </div>
+                                        
+                                      </div>
+                                    </div>
+                                  </div>
+                                </td>
+                                </tr>
+                                @endforeach
+                               
+                                  @foreach ($cali as $xd)
+                                 
+                                 <tr> <td> calificion {{ $xd->calificacion }}</td></tr>
+                                  
+                                    @endforeach
+                                  
+                            </tbody>
+                            </table>
+                            
+                              </div>
+                          
+                    
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
+             
+                </div>
+              
+              </td>
+          
+              </tr>
+              @endforeach
+              
+                 
+           
+            </tbody>
+           
+          </table>
+       
+        </div>
+        </div>
+                <!-- Modal -->
+               <script>
+              function abrirSegundaModal(idModal) {
+  var modalElement = document.querySelector(idModal);
+  var modalInstance = new bootstrap.Modal(modalElement);
+  modalInstance.show();
+} 
+               </script>
+          
+         
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
